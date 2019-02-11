feb2eb1d3cdbccb090e0ab7c6c02df4f2df3f4a1
ashishkg0022 <ashishkg0022@gmail.com>
2019-02-12 00:27:07 +0530

    change in trie

--- intro_pattern_matching.py
+++ intro_pattern_matching.py
@@ -1,16 +1,20 @@
 class Trie:
     
     def __init__(self, dna=None):
+
         self.count = 1
         self.root = [self.count,{}]
 
         if isinstance(dna, list):
+
             for i in dna:
                 self.insert(i)
+
         elif isinstance(dna, str):
             self.insert(dna)
 
     def insert(self, dna):
+
         node = self.root
         for char in dna:
             if not char in node[1]:
@@ -24,6 +28,7 @@ class Trie:
             root = self.root
         
         for char, node in root[1].items():
+
             print(root[0], node[0], char)
             self.print_adjacency_list(node)
 
@@ -35,11 +40,25 @@ T = Trie(['ATAGA','ATC','GAT'])
 T.print_adjacency_list()
 
 '''
-Or,  it can also be wriiten as
+It prints:
 
->>> T = Trie()
->>> T.insert('ATAGA')
->>> T.insert('ATC')
->>> T.insert('GAT')
->>> T.print_adjacency_list()
+1 8 G
+8 9 A
+9 10 T
+1 2 A
+2 3 T
+3 7 C
+3 4 A
+4 5 G
+5 6 A
+
+=========================
+Or,  Trie can also be used in the following way. Same output comes.
+
+T = Trie()
+T.insert('ATAGA')
+T.insert('ATC')
+T.insert('GAT')
+T.print_adjacency_list()
+
 '''
